61910e1150ba5fe33fc761869fac6a89d25a92d0
min1992 <38031850+min1992@users.noreply.github.com>
2021-01-06 15:08:40 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python /biocluster/data/biobk/user_test/chenhuimin/20.other/reverse/main_v2.py --refseq /biocluster/data/biobk/user_test/dongyongfang/database/humandb/hg19_refGene.txt --reference /biocluster/data/bioexec/database/genome/Homo_sapiens_assembly19/v1/Homo_sapiens_assembly19.fasta --variant "NM_007294:c.4548-2A>C"
+python main_v2.py --refseq hg19_refGene.txt --reference Homo_sapiens_assembly19.fasta --variant "NM_007294:c.4548-2A>C"
